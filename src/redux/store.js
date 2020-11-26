7cb956c69ed5dee48e2f4380a4dafed8ab257028
maymax <maymax@maymaxs-iMac.local>
2020-11-26 14:45:10 -0500

    Add redux store

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
+const store = createStore(
+    combineReducers({...reducers}),
+    applyMiddleware(thunk)
+);
+
+export default store;
