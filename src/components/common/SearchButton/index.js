7b52f00d501e1aa34905d32b09a902987bf5dc01
maymax <maymax@maymaxs-iMac.local>
2020-11-27 13:46:29 -0500

    Add basic SearchButton

---
+++ src/components/common/SearchButton/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const SearchButton = () => {
+  return (
+    <View>
+      <Text>SearchButton</Text>
+    </View>
+  );
+};
+
+export default SearchButton;
