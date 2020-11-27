56c2514c69660832b1a4976568651d32da9a4189
maymax <maymax@maymaxs-iMac.local>
2020-11-27 13:08:33 -0500

    Install babel-resolver & set alias for path

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import Logo from '../../components/common/Logo';
-import SearchBar from '../../components/common/SearchBar';
+import Logo from '@components/common/Logo';
+import SearchBar from '@components/common/SearchBar';
 import styles from './style';
 
 function App() {
