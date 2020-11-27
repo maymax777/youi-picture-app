cff06b9423686b5e9501753866704dc0623fad67
maymax <maymax@maymaxs-iMac.local>
2020-11-26 22:37:45 -0500

    Export styles from index.js to style.js

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import Logo from '../../components/common/Logo';
-
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#e6e7e7",
-        flex: 1
-    }
-});
+import styles from './style'
 
 function App() {
     return (
---
+++ src/containers/App/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#e6e7e7",
+        flex: 1
+    }
+});
+
+export default styles;
