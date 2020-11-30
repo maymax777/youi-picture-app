d9c35f91b8c0a9d530a416b1a01a937e4c7052f2
maymax <maymax@maymaxs-iMac.local>
2020-11-30 07:23:02 -0500

    Add keyword handler

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
 import SearchBar from '@components/common/Search/SearchBar';
 import SearchButton from '@components/common/Search/SearchButton';
 import styles from './style';
 
 function Search() {
-  const handleSearchButton = () => {};
+  const [keyword, setKeyword] = useState('');
+  const handleSearchButton = () => {
+    console.log('keyword: ', keyword);
+  };
 
   return (
     <View style={styles.container}>
-      <SearchBar />
+      <SearchBar handleKeyword={setKeyword} />
       <SearchButton handleOnPress={handleSearchButton} />
     </View>
   );
