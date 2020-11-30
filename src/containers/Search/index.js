66856fbb216d9a80cb14e2f16f9e76804a6da542
maymax <maymax@maymaxs-iMac.local>
2020-11-30 06:36:19 -0500

    Fix style in seasrch container

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import SearchBar from '@components/common/Search/SearchBar';
 import SearchButton from '@components/common/Search/SearchButton';
+import styles from './style';
 
 function Search() {
+  const handleSearchButton = () => {};
+
   return (
-    <View>
+    <View style={styles.container}>
       <SearchBar />
-      <SearchButton />
+      <SearchButton handleOnPress={handleSearchButton} />
     </View>
   );
 }
