9ca6a0419a20e28dc1f2daa9b58e2814e20d30f2
maymax <maymax@maymaxs-iMac.local>
2020-12-02 07:54:26 -0500

    Add searchImages to unsplash services

--- src/redux/unsplash/reducer.js
+++ src/redux/unsplash/reducer.js
@@ -1,27 +1,8 @@
 import actions from './actions';
 import Unsplash, { toJson } from 'unsplash-js';
-import { UNSPLASH_API_KEY, API_TIME_LIMIT } from '@utils/config';
 
 const initState = {
   images: [],
-};
-
-const searchImages = (keyword) => {
-  const unsplashApi = new Unsplash({
-    accessKey: UNSPLASH_API_KEY,
-    timeout: API_TIME_LIMIT,
-  });
-
-  return (dispatch) =>
-    unsplashApi.search
-      .photos(keyword, 1, 10)
-      .then(toJson)
-      .then((json) => {
-        return dispatch({
-          type: actions.SEARCH_IMAGE,
-          payload: json.results,
-        });
-      });
 };
 
 function unsplashReducer(state = initState, action) {
--- src/services/unsplash/index.js
+++ src/services/unsplash/index.js
@@ -1,8 +1,17 @@
-import Unsplash from 'unsplash-js';
+import Unsplash, { toJson } from 'unsplash-js';
+import { UNSPLASH_API_KEY, API_TIME_LIMIT } from '@utils/config';
 
 const unsplashApi = new Unsplash({
   accessKey: UNSPLASH_API_KEY,
   timeout: API_TIME_LIMIT,
 });
+
+export const searchImages = async (keyword) => {
+  return unsplashApi.search
+    .photos(keyword, 1, 10)
+    .then(toJson)
+    .then((json) => json.result)
+    .catch((error) => error);
+};
 
 export default unsplashApi;
