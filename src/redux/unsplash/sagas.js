9d255343e041a06c4900ff6fa9d540a1cb01e4d9
maymax <maymax@maymaxs-iMac.local>
2020-12-02 07:26:21 -0500

    Add unsplash sagas

---
+++ src/redux/unsplash/sagas.js
@@ -0,0 +1,15 @@
+import { all, put, takeEvery } from 'redux-saga/effects';
+
+import { put } from 'redux-saga/effects';
+import actions from './actions';
+
+export function* SEARCH({ payload }) {
+  yield put({
+    type: actions.SET_STATE,
+    payload,
+  });
+}
+
+export default function* rootSaga() {
+  yield all([takeEvery(actions.SEARCH_IMAGE, SEARCH)]);
+}
