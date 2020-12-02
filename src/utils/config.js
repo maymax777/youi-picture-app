ff07bd518500f5af1a4647bb9ec291a4437dfe9e
maymax <maymax@maymaxs-iMac.local>
2020-12-02 06:27:53 -0500

    Import unsplash access key from env

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,4 @@
-export const UNSPLASH_API_KEY = '';
-export const API_TIME_LIMIT = '';
+import { UNSPLASH_API_ACCESS_KEY } from '@env';
+
+export const UNSPLASH_API_KEY = `${UNSPLASH_API_ACCESS_KEY}`;
+export const API_TIME_LIMIT = 500;
