2def3907f4b88b7cbbc0ee95ec1b9e39da6e5eef
maymax <maymax@maymaxs-iMac.local>
2020-12-02 07:18:51 -0500

    Update store with saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,17 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers';
+import rootSaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [thunk, sagaMiddleware];
 
 const store = createStore(
   combineReducers({ ...reducers }),
-  applyMiddleware(thunk),
+  applyMiddleware(...middlewares),
 );
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
