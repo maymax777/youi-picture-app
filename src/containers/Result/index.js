a383d0ca9d3cc3a3b96faa297091d16e60d02744
maymax <maymax@maymaxs-iMac.local>
2020-12-06 14:57:05 -0500

    Add renderItem method

--- src/containers/Result/index.js
+++ src/containers/Result/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  ImageBackground,
+  View,
+  Text,
+  SafeAreaView,
+  VirtualizedList,
+} from 'react-native';
+import { useSelector } from 'react-redux';
+import styles from './style';
 
 const Result = () => {
+  const images = useSelector((state) => state.images);
+
+  const renderItem = (item) => {
+    <View style={styles.item}>
+      <ImageBackground source={item.image} style={styles.itemBackground}>
+        <Text style={styles.itemTitle}>{item.title}</Text>
+      </ImageBackground>
+    </View>;
+  };
+
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <SafeAreaView>
+      <VirtualizedList data={images} renderItem={renderItem} />
+    </SafeAreaView>
   );
 };
 
