5fe8656a5b6bb40a2df3b14578f7d2a7cc873ae2
maymax <maymax@maymaxs-iMac.local>
2020-12-06 19:02:15 -0500

    Import Card component and replace renerItem

--- src/containers/Result/index.js
+++ src/containers/Result/index.js
@@ -7,17 +7,14 @@ import {
   VirtualizedList,
 } from 'react-native';
 import { useSelector } from 'react-redux';
+import Card from '@components/common/Card';
 import styles from './style';
 
 const Result = () => {
   const images = useSelector((state) => state.images);
 
   const renderItem = (item) => {
-    <View style={styles.item}>
-      <ImageBackground source={item.image} style={styles.itemBackground}>
-        <Text style={styles.itemTitle}>{item.title}</Text>
-      </ImageBackground>
-    </View>;
+    return <Card item={item} />;
   };
   const getItem = (item) => item;
   const getItemCount = () => (images || []).length;
