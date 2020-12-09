d2ec6b781fde62e190883c751523ba6cd0903802
maymax <maymax@maymaxs-iMac.local>
2020-12-09 13:26:16 -0500

    Add BackHandler

--- src/containers/Gallery/index.js
+++ src/containers/Gallery/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import { SafeAreaView, ScrollView, FlatList } from 'react-native';
+import React, { useEffect } from 'react';
+import { SafeAreaView, FlatList, BackHandler } from 'react-native';
 import { useSelector } from 'react-redux';
 import Card from '@components/common/Card';
 import styles from './style';
 import { COLUMN_SIZE } from '@utils/config';
 
-const Result = () => {
+const Result = ({ navigation }) => {
   const { images, loading } = useSelector((state) => state.Unsplash);
 
   const renderItem = ({ item, index }) => {
     return <Card item={item} key={index} />;
   };
+
+  const onBackPressed = () => {
+    navigation.navigate('Search');
+    return true;
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', onBackPressed);
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', onBackPressed);
+    };
+  });
 
   return (
     <SafeAreaView style={styles.container}>
