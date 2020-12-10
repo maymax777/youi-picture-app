e79ae159e8f06187cab2d40e3e53bd407c897b51
maymax <maymax@maymaxs-iMac.local>
2020-12-10 08:06:32 -0500

    Add VideoRef and hls

--- src/containers/Playback/index.js
+++ src/containers/Playback/index.js
@@ -1,15 +1,28 @@
 import React, { createRef, useEffect } from 'react';
-import { Composition, TimelineRef } from '@youi/react-native-youi';
+import { Composition, TimelineRef, VideoRef } from '@youi/react-native-youi';
+import { View } from 'react-native';
 
 const Playback = () => {
-  const inTimeline = createRef();
-
-  useEffect(() => {}, []);
+  const videoPlayerRef = createRef();
+  const onPlayerReady = () => {
+    if (videoPlayerRef.current) videoPlayerRef.current.play();
+  };
 
   return (
-    <Composition source="Main">
-      <TimelineRef name="In" ref={inTimeline} />
-    </Composition>
+    <View style={{ flex: 1 }}>
+      <Composition source="Player_Main">
+        <VideoRef
+          name="Video-Surface-View"
+          source={{
+            uri:
+              'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8',
+            type: 'HLS',
+          }}
+          ref={videoPlayerRef}
+          onReady={onPlayerReady}
+        />
+      </Composition>
+    </View>
   );
 };
 
