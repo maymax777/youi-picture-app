39504a5a444439475ec24c4dce0c332a6894c773
maymax <maymax@maymaxs-iMac.local>
2020-12-09 20:44:06 -0500

    Add playback navigator and initialize

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -5,11 +5,18 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import Search from '@containers/Search';
 import Gallery from '@containers/Gallery';
+import Playback from '@containers/Playback';
 
-const rootStack = createSwitchNavigator({
-  Search: { screen: Search, navigationOptions: {} },
-  Gallery: { screen: Gallery, navigationOptions: {} },
-});
+const rootStack = createSwitchNavigator(
+  {
+    Search: { screen: Search, navigationOptions: {} },
+    Gallery: { screen: Gallery, navigationOptions: {} },
+    Playback: { screen: Playback, navigationOptions: {} },
+  },
+  {
+    initialRouteName: 'Playback',
+  },
+);
 
 const AppNavigator = createAppContainer(rootStack);
 
