b65b9b6b255514cdd13d8a53d5483c86654884ae
maymax <maymax@maymaxs-iMac.local>
2020-12-10 15:22:27 -0500

    Auto hide timeline bar after some seconds

--- src/containers/Playback/PlaybackControls/index.js
+++ src/containers/Playback/PlaybackControls/index.js
@@ -2,6 +2,8 @@ import React, { createRef, useEffect } from 'react';
 import { Composition, TimelineRef, ViewRef } from '@youi/react-native-youi';
 import styles from '../style';
 
+const TIMELINE_VISIBLE_LIMIT = 15000;
+
 function PlaybackControls() {
   const inTimelineRef = createRef();
   const outTimelineRef = createRef();
@@ -10,12 +12,25 @@ function PlaybackControls() {
     if (inTimelineRef.current) inTimelineRef.current.play();
   });
 
+  /**
+   * Auto hide timeline bar after some seconds
+   * @TIMELINE_VISIBLE_LIMIT
+   */
+  const inTimelineCompleted = () => {
+    setTimeout(() => {
+      if (outTimelineRef.current) outTimelineRef.current.play();
+    }, TIMELINE_VISIBLE_LIMIT);
+  };
   return (
     <Composition
       source="Player_Playback-Controls"
       style={{ position: 'absolute' }}
     >
-      <TimelineRef name="In" ref={inTimelineRef} />
+      <TimelineRef
+        name="In"
+        ref={inTimelineRef}
+        onCompleted={inTimelineCompleted}
+      />
       <TimelineRef name="Out" ref={outTimelineRef} />
       <ViewRef name="Btn-Back-Container" />
       <ViewRef name="Placeholder-Time" />
--- src/containers/Playback/index.js
+++ src/containers/Playback/index.js
@@ -1,11 +1,25 @@
-import React, { useState } from 'react';
-import { ButtonRef, View } from '@youi/react-native-youi';
+import React, { useState, useEffect } from 'react';
+import { ButtonRef, View, Input } from '@youi/react-native-youi';
 import VideoView from './VideoView';
 import PlaybackControls from './PlaybackControls';
 import styles from './style';
 
 function Playback() {
   const [loading, setLoading] = useState(true);
+  const keys = ['Space', 'Play', 'MediaPlay', 'MediaPlayPause'];
+
+  useEffect(() => {
+    keys.forEach((key) => Input.addEventListener(key, registerUserActivity));
+    return () => {
+      keys.forEach((key) =>
+        Input.removeEventListener(key, registerUserActivity),
+      );
+    };
+  }, []);
+
+  const registerUserActivity = (keyEvent) => {
+    console.log('here', keyEvent);
+  };
 
   return (
     <View style={styles.container}>
