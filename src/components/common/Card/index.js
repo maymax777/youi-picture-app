3e799fca5c8f87c1ac04788ae52216df9f03bf91
maymax <maymax@maymaxs-iMac.local>
2020-12-11 08:29:15 -0500

    Navigate to video playback on image card press

--- src/components/common/Card/index.js
+++ src/components/common/Card/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { View, Text, ImageBackground, TouchableOpacity } from 'react-native';
 import styles from './style';
 
-const Card = ({ item }) => {
+const Card = ({ item, navigation }) => {
+  const handleCardPress = () => {
+    navigation.navigate('Playback');
+  };
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={handleCardPress}>
       <ImageBackground source={{ uri: item.image }} style={styles.background}>
         <Text style={styles.title} numberOfLines={1}>
           {item.title}
