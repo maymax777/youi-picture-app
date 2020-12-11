d6bf0cfa1bd2034234d97030dfcd8514f8408242
maymax <maymax@maymaxs-iMac.local>
2020-12-11 04:44:28 -0500

    Add empty button click handler to playpause

--- src/containers/Playback/PlaybackControls/PlayPause/index.js
+++ src/containers/Playback/PlaybackControls/PlayPause/index.js
@@ -1,8 +1,10 @@
 import React, { createRef, useEffect } from 'react';
-import { TimelineRef, ViewRef } from '@youi/react-native-youi';
+import { ButtonRef, TimelineRef, ViewRef } from '@youi/react-native-youi';
 
 export default function PlayPause() {
   const inTimelineRef = createRef();
+
+  const handlePlayPause = () => {};
 
   useEffect(() => {
     if (inTimelineRef.current) inTimelineRef.current.play();
@@ -11,6 +13,7 @@ export default function PlayPause() {
   return (
     <ViewRef name="PlayPause-Container">
       <TimelineRef name="In" ref={inTimelineRef} />
+      <ButtonRef name="Btn-PlayPause" onPress={handlePlayPause} />
     </ViewRef>
   );
 }
