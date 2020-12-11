faef41fc0e6128a101bd8743226a2e14a7fd7746
maymax <maymax@maymaxs-iMac.local>
2020-12-10 21:48:03 -0500

    Remove junk code from Time

--- src/containers/Playback/PlaybackControls/Time/index.js
+++ src/containers/Playback/PlaybackControls/Time/index.js
@@ -2,11 +2,5 @@ import React, { createRef, useEffect } from 'react';
 import { TimelineRef, TextRef } from '@youi/react-native-youi';
 
 export default function Time() {
-  const inTimelineRef = createRef();
-
-  useEffect(() => {
-    if (inTimelineRef.current) inTimelineRef.current.play();
-  });
-
   return <TextRef name="Placeholder-Time" text="00:00" />;
 }
