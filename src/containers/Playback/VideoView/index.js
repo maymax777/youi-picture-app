8f57b2bb10d3fed4e4c1f7161539678440bef919
maymax <maymax@maymaxs-iMac.local>
2020-12-10 20:39:19 -0500

    Export playlist and type to config

--- src/containers/Playback/VideoView/index.js
+++ src/containers/Playback/VideoView/index.js
@@ -1,12 +1,15 @@
 import React, { createRef, useEffect } from 'react';
 import { Composition, VideoRef } from '@youi/react-native-youi';
+import { VIDEO_FORMAT, PLAYLIST } from '@utils/config';
 import styles from '../style';
 
 function VideoView() {
   const videoPlayerRef = createRef();
 
   const onPlayerReady = () => {
-    if (videoPlayerRef.current) videoPlayerRef.current.play();
+    if (videoPlayerRef.current) {
+      videoPlayerRef.current.play();
+    }
   };
 
   return (
@@ -14,9 +17,8 @@ function VideoView() {
       <VideoRef
         name="Video-Surface-View"
         source={{
-          uri:
-            'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8',
-          type: 'HLS',
+          uri: PLAYLIST,
+          type: VIDEO_FORMAT,
         }}
         ref={videoPlayerRef}
         onReady={onPlayerReady}
--- src/utils/config.js
+++ src/utils/config.js
@@ -5,3 +5,6 @@ export const API_TIME_LIMIT = 1000;
 export const UNSPLASH_API_ENDPOINT = `https://api.unsplash.com/search`;
 export const COLUMN_SIZE = 4;
 export const IMAGE_PER_PAGE = 24;
+export const VIDEO_FORMAT = 'HLS';
+export const PLAYLIST =
+  'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8';
